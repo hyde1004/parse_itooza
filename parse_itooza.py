32217fd26988bb4980daab550d02620185760e29
unknown <junggu.lee@PTDMF10-NA105C3.LGE.NET>
2016-05-06 13:26:14 +0900

    table row parse

--- parse_itooza.py
+++ parse_itooza.py
@@ -1,7 +1,31 @@
 import bs4
 import requests
 
-data = requests.get('http://search.itooza.com/index.htm#indexTable2')
-data.encoding='cp949'
-data = bs4.BeautifulSoup(data.text)
-print(data)
+response = requests.get('http://search.itooza.com/index.htm#indexTable2')
+response.encoding='cp949'
+data = bs4.BeautifulSoup(response.text, 'lxml')
+
+'''
+<body>
+  <div id='wrap'>
+    <div id='container'>
+      <div id='content'>
+        <div id='indexTable'>
+          <div id='indexTable2'>
+          	<table border='1' class='ex'>
+          	  <tbody>
+          	    <tr>
+          	      <th><abbr title='주당 배당금'>주당 배당금</abbr></th>
+          	      <td>1,537</td>
+          	      <td>1,682</td>
+          	    </tr>
+</body>
+
+'''
+table_body = data.tbody
+
+rows = table_body.find_all('tr')
+
+for row in rows:
+	if (row.th.string == '주당 배당금'):
+		print(row.th)
